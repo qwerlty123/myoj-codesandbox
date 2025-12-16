164111d71941b9971e3fae9c4cc3e66e357be1d1
qwerlty123 <1787129184@qq.com>
2025-12-16 16:26:20 +0800

    项目创建

---
+++ src/main/java/com/example/myojcodesandbox/CodesandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.myojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodesandboxApplication.class, args);
+    }
+
+}
