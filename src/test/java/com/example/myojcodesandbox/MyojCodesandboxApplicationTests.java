c0ecd61c654da0c000a9b18228bd78cf6929d1c3
qwerlty123 <1787129184@qq.com>
2026-01-21 21:07:40 +0800

    项目创建

---
+++ src/test/java/com/example/myojcodesandbox/MyojCodesandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyojCodesandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
